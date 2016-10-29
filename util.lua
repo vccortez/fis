78c3ec52a865d5f76cb7595a8c2502dde2b73bce
Vitor Cortez <vitoracortez+github@gmail.com>
2016-10-29 11:26:11 -0300

    Update method's functional implementation
    Add `head`, `tail` and `reduce` functional methods to simplify reading.

--- util.lua
+++ util.lua
@@ -1,43 +1,55 @@
-local function _min_(a, b)
+local function head(tbl)
+  return tbl[1]
+end
+
+local function tail(tbl)
+  return {table.unpack(tbl, 2)}
+end
+
+local function fold(fn, tbl, val)
+  for _, v in pairs(tbl) do
+    val = fn(val, v)
+  end
+  return val
+end
+
+local function reduce(fn, tbl)
+  return fold(fn, tail(tbl), head(tbl))
+end
+
+local function minimum(a, b)
   if a ~= a then return b
   elseif b ~= b then return a
   else return math.min(a, b)
   end
 end
 
-local function min(params)
-  if #params == 0 then
-    return math.huge
-  else
-    local a = params[1]
-    return _min_(a, min({table.unpack(params, 2)}))
-  end
+local function min(...)
+  return reduce(minimum, {...})
 end
 
-local function _max_(a, b)
+local function maximum(a, b)
   if a ~= a then return b
   elseif b ~= b then return a
   else return math.max(a, b)
   end
 end
 
-local function max(params)
-  if #params == 0 then
-    return -math.huge
-  else
-    local a = params[1]
-    return _max_(a, max({table.unpack(params, 2)}))
-  end
+local function max(...)
+  return reduce(maximum, {...})
 end
 
 local function bound(x, a, b)
   if a > b then a, b = b, a end
 
-  return max({a, min({x, b})})
+  return max(a, min(x, b))
 end
 
 return {
   bound = bound,
   min = min,
   max = max,
+  head = head,
+  tail = tail,
+  reduce = reduce,
 }
