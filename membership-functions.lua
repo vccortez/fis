a98011ada2a5ca97d45f57e40996d7d51cc4f6e7
Vitor Cortez <vitor.a.cortez@gmail.com>
2016-10-30 15:30:38 -0300

    Change functions to module scope

--- membership-functions.lua
+++ membership-functions.lua
@@ -1,6 +1,6 @@
 local u = require 'util'
 
-local function gaussian(x, params)
+function gaussmf(x, params)
   if x == 'name' then return 'gaussian' end
 
   local sig, c = table.unpack(params, 1, 2)
@@ -8,7 +8,7 @@ local function gaussian(x, params)
   return math.exp(-((x - c)^2) / 2 * sig^2)
 end
 
-local function trapezoidal(x, params)
+function trapmf(x, params)
   if x == 'name' then return 'trapezoidal' end
 
   local a, b, c, d = table.unpack(params, 1, 4)
@@ -16,16 +16,10 @@ local function trapezoidal(x, params)
   return u.max(0, u.min( (x - a)/(b - a), 1, (d - x)/(d - c) ))
 end
 
-local function triangular(x, params)
+function trimf(x, params)
   if x == 'name' then return 'triangular' end
 
   local a, b, c = table.unpack(params, 1, 3)
 
   return u.max(0, u.min( (x - a)/(b - a), (c - x)/(c - b) ))
 end
-
-return {
-  trimf   = triangular,
-  trapmf  = trapezoidal,
-  gaussmf = gaussian,
-}
