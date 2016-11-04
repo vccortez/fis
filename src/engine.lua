d82d98dbd3dfef1b4b67c9a0e98035103cc3c32c
Vitor Cortez <vitor.a.cortez@gmail.com>
2016-11-04 12:52:38 -0300

    Implement fuzzy aggregation procedure

--- src/engine.lua
+++ src/engine.lua
@@ -1,6 +1,7 @@
 local variables = require 'variables'
 local rules = require 'rules'
 local op = require 'operators'
+local util = require 'util'
 
 local function create_engine(params)
   -- initialize internal state with default values
@@ -74,7 +75,7 @@ local function create_engine(params)
 
       -- generate the set with each discrete points
       local discrete_set = {}
-      for i = 0, v.disc - 1 do
+      for i = 0, output.disc - 1 do
         discrete_set[i + 1] = (i * output.step) + output.min
       end
 
@@ -90,20 +91,63 @@ local function create_engine(params)
       end
     end
 
+    local activation_degree = {}
     for i, rule in ipairs(state.rules) do
       activation_degree[i] = rule.get_activation_degree(crips_inputs, state.conjunction, state.disjunction, state.complement)
 
       truncated_output_set[i] = rule.get_output_set(activation_degree[i], state.implication, original_output_set)
     end
 
-    local aggregated = {}
-    for i, out in ipairs(state.outputs) do
-      aggregated[out.name] = aggregate(truncated_output_set, out.name, state.aggregation)
+    --[[ TODO: remove printing
+    for i, v in ipairs(truncated_output_set) do
+      io.write('\nrule: ' .. i)
+
+      for varn, set in pairs(v) do
+        io.write('\nvar name: ' .. varn ..'\n')
+        for j, elem in ipairs(set) do
+          io.write('#'..j..' = ('..elem[1]..' / '..elem[2]..'), ')
+        end
+      end
+      io.write('\n')
     end
+    ]]
+
+    local aggregated = {}
+    for _, out in ipairs(state.outputs) do
+      -- aggregated[out.name] = aggregate(truncated_output_set, out.name, state.aggregation)
+      aggregated[out.name] = {}
+
+      for i = 0, out.disc - 1 do
+        aggregated[out.name][i + 1] = { (i * out.step) + out.min, {} }
+      end
+
+      for _, set in ipairs(truncated_output_set) do
+        if set[out.name] then
+          for i, elem in ipairs(set[out.name]) do
+            table.insert(aggregated[out.name][i][2], elem[2])
+          end
+        end
+      end
+
+      for i, v in ipairs(aggregated[out.name]) do
+        aggregated[out.name][i][2] = util.reduce(state.aggregation, aggregated[out.name][i][2])
+      end
+    end
+
+    --[[ TODO: remove printing
+    for vname, set in pairs(aggregated) do
+      io.write('\naggregated set of ' .. vname .. '\n')
+
+      for j, elem in ipairs(set) do
+        io.write('#'..j..' = ('..elem[1]..' / '..elem[2]..'), ')
+      end
+      io.write('\n')
+    end
+    ]]
 
     local crisp_outputs = {}
     for key, set in pairs(aggregated) do
-      crisp_outputs[key] = defuzzify(set, state.defuzzification)
+      crisp_outputs[key] = state.defuzzification(set)
     end
 
     return crisp_outputs
