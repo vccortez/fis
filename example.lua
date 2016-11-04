061531b124662397ca4f3dc59cb5e6b1e30087fb
Vitor Cortez <vitor.a.cortez@gmail.com>
2016-11-04 12:53:03 -0300

    Update tipping problem example

--- example.lua
+++ example.lua
@@ -1,7 +1,6 @@
 -- --------------------------------------------------------
--- runs the classic tipper fuzzy inference system
+-- runs the classic tipping fuzzy inference system
 -- --------------------------------------------------------
-
 
 local fis = require 'init'
 local fn = fis.mfn
@@ -9,25 +8,38 @@ local op = fis.ops
 local If = fis.rules.build_rule
 local P = fis.props.create_proposition
 
-local engine = fis.create_engine{'tipper', op.Min, op.Max, op.Not}
+local engine = fis.create_engine{name = 'tipper'}
 
-local service = engine.add_input('service', 0., 10.)
-  .add_term('poor', fn.gaussmf, {1.5, 0.})
-  .add_term('good', fn.gaussmf, {1.5, 5.})
-  .add_term('excellent', fn.gaussmf, {1.5, 10.})
+local service = engine.add_input('service', 0., 10., 11)
+  .add_term('poor', fn.trimf, {0., 0., 5.})
+  .add_term('good', fn.trimf, {0., 5., 10.})
+  .add_term('excellent', fn.trimf, {5., 10., 10.})
 
-local food = engine.add_input('food', 0., 10.)
-  .add_term('rancid', fn.trapmf, {0., 0., 1., 3.})
-  .add_term('delicious', fn.trapmf, {7., 9., 10., 10.})
+local food = engine.add_input('food', 0., 10., 11)
+  .add_term('rancid', fn.trimf, {0., 0., 5.})
+  .add_term('average', fn.trimf, {0., 5., 10.})
+  .add_term('delicious', fn.trimf, {5., 10., 10.})
 
-local tip = engine.add_output('tip', 0., 30.)
-  .add_term('cheap', fn.trimf, {0., 5., 10.})
-  .add_term('average', fn.trimf, {10., 15., 20.})
-  .add_term('generous', fn.trimf, {20., 25., 30.})
+local tip = engine.add_output('tip', 0., 25., 26)
+  .add_term('cheap', fn.trimf, {0., 0., 13.})
+  .add_term('average', fn.trimf, {0., 13., 25.})
+  .add_term('generous', fn.trimf, {13., 25., 25.})
 
 local rules = engine.add_rules{
-  If( (P(service, 'poor') & P(food, 'rancid')) ).Then{{tip, 'cheap'}},
+  If( P(service, 'poor') & P(food, 'rancid') ).Then{{tip, 'cheap'}},
+  If( P(service, 'good') ).Then{{tip, 'average'}},
+  If( P(service, 'excellent') | P(food, 'delicious') ).Then{{tip, 'generous'}}
 }
+
+print'type service input [0-10]'
+local serv = tonumber(io.read())
+print'type food input [0-10]'
+local food = tonumber(io.read())
+local outputs = engine.process{service = serv, food = food}
+
+for k, v in pairs(outputs) do
+  print(k, v)
+end
 
 --[[
 io.write'type a value to evaluate service: '
@@ -41,5 +53,3 @@ end
 
 -- food:evaluate(10)
 ]]
-
-print(engine.to_string())
